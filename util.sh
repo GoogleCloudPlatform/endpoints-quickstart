3e8fbe59710d50574dfebc4ba43ba439cae34746
Paul Buser <buser@google.com>
2017-04-12 12:25:06 -0700

    removed unused prompt() method

--- util.sh
+++ util.sh
@@ -22,29 +22,3 @@ check_devshell() {
     exit 1
   fi
 }
-
-prompt() {
-  # Prompts the user to enter "yes" or "no".
-  # If they enter "yes", then $1 is executed as a command.
-  if [[ -z "$1" ]]; then
-    echo "Error: no command specified for prompt. Exiting."
-    exit 1
-  fi
-  while true; do
-    read -p "Shall I proceed? " prompt
-    case "$prompt" in
-      [Yy]* )
-        # Not quoted because this should be interpreted as multiple tokens.
-        $1
-        break
-        ;;
-      [Nn]* )
-        echo "Not continuing."
-        exit 1
-        ;;
-      * )
-        echo "Please enter yes or no."
-        ;;
-    esac
-  done
-}
