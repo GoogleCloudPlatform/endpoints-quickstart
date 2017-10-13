c0a7786a885d12afd597288af6e6d4e779e843f8
Paul Buser <buser@google.com>
2017-10-12 17:20:14 -0700

    Fix bug where no API key used; collect HTTP codes when generating traffic.

--- scripts/generate_traffic.sh
+++ scripts/generate_traffic.sh
@@ -17,20 +17,45 @@ set -euo pipefail
 
 source util.sh
 
+# Use this to keep track of what HTTP status codes we receive.
+declare -A codes
+
+# generate_traffic will print a status update every UPDATE_FREQUENCY messages.
+UPDATE_FREQUENCY=25
+
 main() {
   # Get our working project, or exit if it's not set.
   local project_id=$(get_project_id)
   if [[ -z "$project_id" ]]; then
     exit 1
   fi
-  local url="https://${project_id}.appspot.com/airportName?iataCode=${IATA_CODE}"
+  export url="https://${project_id}.appspot.com/airportName?iataCode=${IATA_CODE}"
   echo "This command will exit automatically in $TIMEOUT_SECONDS seconds."
   echo "Generating traffic to ${url}..."
   echo "Press Ctrl-C to stop."
   local endtime=$(($(date +%s) + $TIMEOUT_SECONDS))
+  local request_count=0
   # Send queries repeatedly until TIMEOUT_SECONDS seconds have elapsed.
   while [[ $(date +%s) -lt $endtime ]]; do
-    curl "$url" &> /dev/null
+    request_count=$(( request_count + 1))
+    if [[ $((request_count % UPDATE_FREQUENCY)) == 0 ]]; then
+      echo "Served ${request_count} requests."
+    fi
+    # Make the HTTP request and save its status in an associative array.
+    http_status=$(curl -so /dev/null -w "%{http_code}" "$url")
+    if [[ "${!codes[@]}" != *"$http_status"* ]]; then
+      codes["$http_status"]="1"
+    else
+      codes["$http_status"]="$(( ${codes[$http_status]} + 1 ))"
+    fi
+  done
+}
+
+print_status() {
+  echo ""
+  echo "HTTP status codes received from ${url}:"
+  for code in "${!codes[@]}"; do
+    echo "${code}: ${codes[$code]}"
   done
 }
 
@@ -47,5 +72,8 @@ else
   echo "Usage: generate_traffic.sh [iata-code]"
   exit 1
 fi
+
+# Print the received codes when we exit.
+trap print_status EXIT
 
 main "$@"
