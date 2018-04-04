06388292c9d71232b8e1b98e6f0f50cf3b01fda6
Simon Zeltser <zeltser@google.com>
2018-04-04 13:25:53 -0700

    Fixing deploy_app script
    Remove customization of configuration Id.

--- scripts/deploy_app.sh
+++ scripts/deploy_app.sh
@@ -30,7 +30,6 @@ main() {
   # Prepare the necessary variables for substitution in our app configuration
   # template, and create a temporary file to hold the templatized version.
   local service_name="${project_id}.appspot.com"
-  local config_id=$(get_latest_config_id "$service_name")
   export TEMP_FILE="${APP}_deploy.yaml"
   < "$APP" \
     sed -E "s/SERVICE_NAME/${service_name}/g" \
